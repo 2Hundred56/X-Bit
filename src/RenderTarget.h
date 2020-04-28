f7df7b92688fae6916a44e81c47d40bee9a16824
triforce <ethan.saff@gmail.com>
2020-04-28 11:34:26 -0400

    Basic RenderTarget

---
+++ src/RenderTarget.h
@@ -0,0 +1,21 @@
+/*
+ * RenderTexture.h
+ *
+ *  Created on: Apr 28, 2020
+ *      Author: triforce
+ */
+
+#ifndef RENDERTARGET_H_
+#define RENDERTARGET_H_
+class IntVector;
+class Texture;
+class RenderTarget {
+	virtual IntVector DestinationSize() = 0;
+	virtual void Render(Texture* tex) = 0;
+	virtual void Flip() = 0;
+	virtual void BeginGraphics() = 0;
+	virtual void Initialize(IntVector destSize) = 0;
+	virtual void End() = 0;
+};
+
+#endif /* RENDERTARGET_H_ */
